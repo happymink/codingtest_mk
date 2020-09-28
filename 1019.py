fbdc403f6c8854cf393c8dc188b56afc9b51b6b8
happymink <71982555+happymink@users.noreply.github.com>
2020-09-29 03:12:13 +0900

    CodeUP 100제
    1010~1018

---
+++ 1019.py
@@ -0,0 +1,4 @@
+a,b,c = input().split(".")
+b = text.zfill(2)
+c = text.zfill(2)
+print("{0}.{1}.{2}".format(a,b,c))
--- codeup/file_make_macro.py
+++ codeup/file_make_macro.py
@@ -1,3 +1,3 @@
-for i in range(1,10):
-    with open("100"+str(i)+".py" ,"w",encoding="utf8") as report_file:
+for i in range(0,10):
+    with open("101"+str(i)+".py" ,"w",encoding="utf8") as report_file:
         report_file.write("민균화이팅")
