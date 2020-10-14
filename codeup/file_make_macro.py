9915ba5630c0512579de8dfc4bd1cfe6b7dd9924
happymink <71982555+happymink@users.noreply.github.com>
2020-10-14 21:11:48 +0900

    1

--- codeup/file_make_macro.py
+++ codeup/file_make_macro.py
@@ -1,3 +1,3 @@
 for i in range(0,10):
-    with open("103"+str(i)+".py" ,"w",encoding="utf8") as report_file:
-        report_file.write("민균화이팅")
+    with open("104"+str(i)+".py" ,"w",encoding="utf8") as report_file:
+        report_file.write("후회없이 열심히")
