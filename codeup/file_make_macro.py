491ca59055342624dfa70867711bec83a110c076
happymink <71982555+happymink@users.noreply.github.com>
2020-10-15 22:49:56 +0900

    make 1051~1060

--- codeup/file_make_macro.py
+++ codeup/file_make_macro.py
@@ -1,3 +1,3 @@
-for i in range(0,10):
-    with open("104"+str(i)+".py" ,"w",encoding="utf8") as report_file:
+for i in range(1,10):
+    with open("105"+str(i)+".py" ,"w",encoding="utf8") as report_file:
         report_file.write("가만히 있으면서 변화를 바라지 말자")
