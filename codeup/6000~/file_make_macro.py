07940000a4da8e4637949451e1de2b59a3f9e151
happymink <71982555+happymink@users.noreply.github.com>
2021-03-08 21:35:46 +0900

    6000
    0308 다시 시작

--- codeup/6000~/file_make_macro.py
+++ codeup/6000~/file_make_macro.py
@@ -1,3 +1,3 @@
 for i in range(1,10):
-    with open("105"+str(i)+".py" ,"w",encoding="utf8") as report_file:
+    with open("600"+str(i)+".py" ,"w",encoding="utf8") as report_file:
         report_file.write("가만히 있으면서 변화를 바라지 말자")
