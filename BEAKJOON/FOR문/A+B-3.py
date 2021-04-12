36a54827fe9bc3275db1f193cfa41ac5e605faae
happymink <71982555+happymink@users.noreply.github.com>
2021-04-13 01:38:11 +0900

    0413
    BEAKJOON

---
+++ BEAKJOON/FOR문/A+B-3.py
@@ -0,0 +1,6 @@
+count = input()
+
+for i in range(0,int(count)):
+    A, B = input().split(" ")
+    print(int(A)+int(B))
+    
--- BEAKJOON/FOR문/구구단.py
+++ BEAKJOON/FOR문/구구단.py
@@ -1,4 +1,7 @@
 #몇 단을 출력할지 N 입력
 N = input()
 
-for 
+for i in range(1, 10):
+    print("{} * {} = {}".format(int(N),i,int(N)*i))
+
+    #range (a,b) a 이상 b 미만 반복
