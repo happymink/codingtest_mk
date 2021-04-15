36fbef2c6a9d6cfa54e5b4b6f95501255f6bd2d3
happymink <71982555+happymink@users.noreply.github.com>
2021-04-15 13:37:49 +0900

    Update 1051.py

--- codeup/1051~1060/1051.py
+++ codeup/1051~1060/1051.py
@@ -1,6 +1,5 @@
-a[3] = {0,0,0}
-b[3] = {0,0,0}
-c[3] = {0,0,0}
+year = input("오늘의 연도를 입력하시오.:")
+month = input("오늘의 월을 입력하시오.:")
+day = input("오늘의 일을 입력하시오.:")
 
-a = input().split(" ")
-    
+print("오늘은 {}년 {}월 {}일입니다.".format(year,month,day))
