a8867ae2ec46b4e9bdf6733e4797075888155ae5
happymink <71982555+happymink@users.noreply.github.com>
2021-04-21 23:38:13 +0900

    Update 1번.py

--- middle term/1번.py
+++ middle term/1번.py
@@ -1,9 +1,16 @@
-radius = int(input("케익의 반지름을 입력하세요:"))
-height = int(input("케익의 높이를 입력하세요:"))
-degree = int(input("케익의 각도를 입력하세요:"))
+radius = int(input("케익의 반지름을 입력하세요: "))
+height = int(input("케익의 높이를 입력하세요: "))
+degree = int(input("케익의 각도를 입력하세요: "))
+
+pi = 3.14
 
 #호의 길이
-length =
-volume = 1/2*(radius*radius)*(degree/90)*height 
-# 1/2 x r^2 x degree 
+length = 2* pi * radius * degree / 360
+
+# v = 1/2 * 반지름 * 호의 길이 * 높이
+volume = 1/2*radius*length*height
+# v = 1/2 * 반지름의 제곱 * 각도/360 * 높이
+volume = pi*radius*radius*degree/360 * height
+
+#소수점 자리수 3자리까지 출력
 print("케익의 부피는 {0:0.3f}입니다".format(volume))
