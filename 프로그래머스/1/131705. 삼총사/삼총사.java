41d1d7ac4f65a11f25a2c6972bcf85acb2f278d0
happymink <71982555+happymink@users.noreply.github.com>
2023-10-24 18:30:57 +0900

    [level 1] Title: 삼총사, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,13 +1,14 @@
 class Solution {
     public int solution(int[] number) {
         int answer = 0;
-        
+        int count = 0;
         for(int i = 0; i<number.length; i++){
             int result = number[i];
             for(int j = i+1; j<number.length; j++){
                 result += number[j];
                 for(int k = j+1; k<number.length; k++){
                     result += number[k];
+                    System.out.print(count++);
                     if(result == 0){
                         answer++;
                         result -= number[k];
